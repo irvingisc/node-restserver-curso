98347055066479617917019287e3fc008e4fa54e
Irving <irvingsalvador92@gmail.com>
2020-05-17 09:07:30 -0500

    Se corrige archivo de configuración

--- server/config/config.js
+++ server/config/config.js
@@ -3,39 +3,42 @@
 // ============================
 process.env.PORT = process.env.PORT || 3000;
 
+
 // ============================
 //  Entorno
 // ============================
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+
 // ============================
-//  Vencimiento token
+//  Vencimiento del Token
 // ============================
-process.env.CADUCIDAD_TOKEN =60 * 60 * 24 * 30;
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
 
 // ============================
 //  SEED de autenticación
 // ============================
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 
-
 // ============================
 //  Base de datos
 // ============================
 let urlDB;
 
-if ( process.env.NODE_ENV === 'dev' )
-	urlDB = 'mongodb://localhost:27017/cafe';
-else
-	urlDB = process.env.MONGO_URI;
-
-//Se modificó variable de conexión para que no sea visible en github
-
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 process.env.URLDB = urlDB;
 
 // ============================
-//  Google client ID
+//  Google Client ID
 // ============================
-
-process.env.CLIENT_ID = process.env.CLIENT_ID || '16036579921-gg7f5qe3r265qbtlvcihookhpau3dfs4.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '16036579921-lp84nclgdhpnol0bnothcj04louj4e93.apps.googleusercontent.com';
+                                                 
